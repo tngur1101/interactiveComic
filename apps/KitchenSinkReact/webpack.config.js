d967d2c45eb2a4307568e1d4c62b73453b9ac639
cauoss4 <65845058+cauoss4@users.noreply.github.com>
2020-06-16 02:08:44 +0900

    Add files via upload

---
+++ apps/KitchenSinkReact/webpack.config.js
@@ -0,0 +1,28 @@
+/*! Rappid v3.1.1 - HTML5 Diagramming Framework - TRIAL VERSION
+
+Copyright (c) 2015 client IO
+
+ 2020-06-11 
+
+
+This Source Code Form is subject to the terms of the Rappid Trial License
+, v. 2.0. If a copy of the Rappid License was not distributed with this
+file, You can obtain one at http://jointjs.com/license/rappid_v2.txt
+ or from the Rappid archive as was distributed by client IO. See the LICENSE file.*/
+
+
+module.exports = {
+    entry: './src/index.tsx',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, use: 'ts-loader' }
+        ]
+    }
+};
